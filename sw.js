a324f3f51487996de8359526ff52769fbbf7b8eb
minhaz <minhazav@gmail.com>
2022-01-09 17:45:55 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,8 +9,8 @@ self.addEventListener('install', function(event) {
             return cache.addAll([
                 '/assets/app.css',
                 '/assets/main.css',
-                '/assets/js/html5-qrcode.min.js',
-                '/assets/js/app.js',
+                // '/assets/js/html5-qrcode.min.js',
+                // '/assets/js/app.js',
                 '/assets/fonts/ibm-plex-sans/ibm-plex-sans-v2-latin-300.woff2',
             ]);
         }).then(function() {
